84b06778b4eeb0aa37c5c8c9ee05a7232ffda8d4
John Francis <johnfrancis13@uchicago.edu>
2021-10-27 12:36:20 +0100

    finished intialization

---
+++ week_4.R
@@ -0,0 +1,19 @@
+library("usethis")
+library("gitcreds")
+
+### Process for setting up a new repo and using github
+
+### Step 1 create a github token 
+# create_github_token()
+# token: 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
+
+### Step 2
+# gitcreds_set()
+# copy in the token
+
+### Step 3
+# need at least one commit prior to this
+# use_github()
+
+### Step 4 
+# now you can start push/pull/commit!
